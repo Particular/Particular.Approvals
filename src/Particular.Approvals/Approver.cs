ae90a8194cf2cad0eb0d80c268b7ab7026ab3ae6
Brandon Ording <bording@gmail.com>
2018-07-09 15:39:33 -0400

    Add string-based Verify

---
+++ src/Particular.Approvals/Approver.cs
@@ -0,0 +1,38 @@
+﻿namespace Particular.Approvals
+{
+    using System.IO;
+    using System.Runtime.CompilerServices;
+    using NUnit.Framework;
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static class Approver
+    {
+        static readonly string approvalFilesPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "..", "..", "..", "ApprovalFiles");
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="callerClassName"></param>
+        /// <param name="callerMethodName"></param>
+        public static void Verify(string text, [CallerFilePath] string callerClassName = null, [CallerMemberName] string callerMethodName = null)
+        {
+            callerClassName = Path.GetFileNameWithoutExtension(callerClassName);
+
+            var receivedFile = Path.Combine(approvalFilesPath, $"{callerClassName}.{callerMethodName}.received.txt");
+            File.WriteAllText(receivedFile, text);
+
+            var approvedFile = Path.Combine(approvalFilesPath, $"{callerClassName}.{callerMethodName}.approved.txt");
+            var approvedText = File.ReadAllText(approvedFile);
+
+            var normalizedApprovedText = approvedText.Replace("\r\n", "\n");
+            var normalizedReceivedText = text.Replace("\r\n", "\n");
+
+            Assert.AreEqual(normalizedApprovedText, normalizedReceivedText);
+
+            File.Delete(receivedFile);
+        }
+    }
+}
---
+++ src/Tests/ApproverTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests
+{
+    using NUnit.Framework;
+    using Particular.Approvals;
+
+    [TestFixture]
+    class ApproverTests
+    {
+        [Test]
+        public void Can_Approve_Text()
+        {
+            Approver.Verify("Text to\r\napprove");
+        }
+    }
+}
